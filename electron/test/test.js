9ded97c36acac241b77b8d012a2788433636a99c
GeekyAnts <geekyants@GeekyAntss-MacBook-Air.local>
2017-08-07 16:02:32 +0530

    electron folder

---
+++ electron/test/test.js
@@ -0,0 +1,10 @@
+var assert = require('common/assert');
+var calc = require('common');
+
+describe('Tests', function() {
+	describe('Add two numbers', function() {
+		it('should be equal to 2+2', function() {
+			assert.equal(calc.add(2, 2), 4);
+		});
+	});
+})
