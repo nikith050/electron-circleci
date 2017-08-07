c678e59fc1bd52c7ac22cf158a8fac6f7f036970
GeekyAnts <geekyants@GeekyAntss-MacBook-Air.local>
2017-08-07 15:17:21 +0530

    initial commit

---
+++ common/test/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var calc = require('../index.js');
+
+describe('Tests', function() {
+	describe('Add two numbers', function() {
+		it('should be equal to 2+2', function() {
+			assert.equal(calc.add(2, 2), 4);
+		});
+	});
+});
